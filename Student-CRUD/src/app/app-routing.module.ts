cb9388ce6de631e8099a7051f7204a58150cf520
Muhammad <md.faisal.mahmud.se@gmail.com>
2024-03-11 11:04:27 +0600

    margin corrected

--- Student-CRUD/src/app/app-routing.module.ts
+++ Student-CRUD/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from '../Components/student/student.component';
+import { StudentDialogComponent } from '../Components/student-dialog/student-dialog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'students', component: StudentComponent },
+  { path: 'add-student', component: StudentDialogComponent },
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
